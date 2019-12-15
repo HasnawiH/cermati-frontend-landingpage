23f7f9970ef4bea5455c8a2d14a597fe90804bbe
awinkh <hasnawihaeba@gmail.com>
2019-12-16 01:45:30 +0700

    add newsletter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, {useState, useEfect} from 'react';
-import Notification from './components/Notification/notification';
-import Header from './components/Header/header';
-import Highlights  from './components/Highlights/highlights'
-import Footer from './components/Footer/footer';
+import React, { useState } from "react";
+import Notification from "./components/Notification/notification";
+import Header from "./components/Header/header";
+import Highlights from "./components/Highlights/highlights";
+import Footer from "./components/Footer/footer";
+import Newsletter from "./components/Newsletter/newsletter";
 
 //CSS
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  const [visible, setVisible] = useState(true)
-  const handleHidden = value =>{
-    setVisible(value)
-  }
-	return ( 
+  const [visible, setVisible] = useState(true);
+  const [open, setOpen] = useState(true);
+
+  const handleHidden = value => {
+    setVisible(value);
+  };
+
+  const handleClose = value => {
+    setOpen(value);
+  };
+
+  return (
     <>
-      { visible ? (
+      {visible ? (
         <>
-        <Notification visibleChange={(value)=> handleHidden(value) }/>
-        <div className='box1'/>
+          <Notification visibleChange={value => handleHidden(value)} />
+          <div className="box1" />
         </>
-      ) : ('')}
-      <Header/>
-      <Highlights/>
-      <Footer/>
+      ) : (
+        ""
+      )}
+      <Header />
+      <Highlights />
+      <Footer />
+      {open ? <Newsletter closeChange={value => handleClose(value)} /> : ""}
     </>
-	);
+  );
 };
 
 export default App;
---
+++ src/components/Newsletter/newsletter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./newsletter.css";
+
+const newsletter = props => {
+  const handleHidden = newValue => {
+    props.closeChange(newValue);
+  };
+  return (
+    <div className="newsletters">
+      <i class="fas fa-times" />
+      <div className="news-text">
+        <h3> Get latest updates in web technologies </h3>
+        <span>
+          I write articles related to web technologies, such as design trends,
+          development tools, UI/UX case studies and reviews, and more. Sign up
+          to my newsletter to get them all.
+        </span>
+      </div>
+      <div className="news-email">
+        <input
+          className="news-input"
+          type="text"
+          name="email"
+          placeholder="Email Address"
+        />
+        <button onClick={() => handleHidden(false)} className="news-button">
+          Count me in!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default newsletter;
